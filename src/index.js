5d7e6d76f0c9817a48ae2d30b8a263dbb9a913a1
Ruslan Menibaev <ruslanmenibaev@gmail.com>
2019-11-24 23:08:47 +0100

    add entrypoint for root component.

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { AppointmentsDayView } from './Appointment'
+
+ReactDOM.render(
+  <AppointmentsDayView appointments={[]} />,
+  document.getElementById("root")
+)
